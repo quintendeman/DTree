779882f4a7e683439cdd4d3561241aa2421bb0e8
Daniel DeLayo <ddelayo@cs.stonybrook.edu>
2024-08-15 12:12:21 -0400

    array based script

--- scripts/mixed_test.sh
+++ scripts/mixed_test.sh
@@ -6,41 +6,48 @@ cd ${base_dir}
 pip install -r requirements
 mkdir -p results
 
-# Test run
-python evaluate_mixed.py kron_13_query10_binary
+#!/bin/bash
 
-# Full sweep
-python evaluate_mixed.py kron_13_query10_binary
-python evaluate_mixed.py kron_15_query10_binary
-python evaluate_mixed.py kron_16_query10_binary
-python evaluate_mixed.py kron_17_query10_binary
-# python evaluate_mixed.py kron_18_query10_binary
+run_test() {
+	python evaluate_mixed.py $1
+}
 
-python evaluate_mixed.py dnc_query10_binary
-python evaluate_mixed.py tech_query10_binary
-python evaluate_mixed.py enron_query10_binary
+declare -a streams=(
+[0]="kron_13_query10_binary"
+[1]="kron_15_query10_binary"
+[2]="kron_16_query10_binary"
+[3]="kron_17_query10_binary"
+[4]="kron_18_query10_binary"
+#
+[5]="dnc_query10_binary"
+[6]="tech_query10_binary"
+[7]="enron_query10_binary"
+#
+[8]="twitter_query10_binary"
+[9]="stanford_query10_binary"
+[10]="random2N_query10_binary"
+[11]="randomNLOGN_query10_binary"
+[12]="randomNSQRTN_query10_binary"
+[13]="randomDIV_query10_binary"
+# Fixed Forest
+[14]="kron_13_ff_query10_binary"
+[15]="kron_15_ff_query10_binary"
+[16]="kron_16_ff_query10_binary"
+[17]="kron_17_ff_query10_binary"
+[18]="kron_18_ff_query10_binary"
+#
+[19]="dnc_ff_query10_binary"
+[20]="tech_ff_query10_binary"
+[21]="enron_ff_query10_binary"
+#
+[22]="twitter_ff_query10_binary"
+[23]="stanford_ff_query10_binary"
+[24]="random2N_ff_query10_binary"
+[25]="randomNLOGN_ff_query10_binary"
+[26]="randomNSQRTN_ff_query10_binary"
+[27]="randomDIV_ff_query10_binary"
+)
 
-python evaluate_mixed.py twitter_query10_binary
-python evaluate_mixed.py stanford_query10_binary
-python evaluate_mixed.py random2N_query10_binary
-python evaluate_mixed.py randomNLOGN_query10_binary
-python evaluate_mixed.py randomNSQRTN_query10_binary
-python evaluate_mixed.py randomDIV_query10_binary
+run_test ${streams[$1]}
 
-# Fixed-forest
-python evaluate_mixed.py kron_13_ff_query10_binary
-python evaluate_mixed.py kron_15_ff_query10_binary
-python evaluate_mixed.py kron_16_ff_query10_binary
-python evaluate_mixed.py kron_17_ff_query10_binary
-# python evaluate_mixed.py kron_18_ff_query10_binary
 
-python evaluate_mixed.py dnc_ff_query10_binary
-python evaluate_mixed.py tech_ff_query10_binary
-python evaluate_mixed.py enron_ff_query10_binary
-
-python evaluate_mixed.py twitter_ff_query10_binary
-python evaluate_mixed.py stanford_ff_query10_binary
-python evaluate_mixed.py random2N_ff_query10_binary
-python evaluate_mixed.py randomNLOGN_ff_query10_binary
-python evaluate_mixed.py randomNSQRTN_ff_query10_binary
-python evaluate_mixed.py randomDIV_ff_query10_binary
